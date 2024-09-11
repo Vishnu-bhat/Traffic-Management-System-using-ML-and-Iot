3cc035671ea5c39e07587792cc44539a26dd3032
Vishnu-bhat <vishnuramesh1105@gmail.com>
2024-09-11 14:19:02 +0530

    integrated deepsort

--- MAIN.py
+++ MAIN.py
@@ -6,53 +6,77 @@ import numpy as np
 
 import math
 
+from deep_sort.deep_sort.tracker import Tracker as DeepSortTracker
+from deep_sort.tools import generate_detections as gdet
+from deep_sort.deep_sort import nn_matching
+from deep_sort.deep_sort.detection import Detection
+import numpy as np
+
+
 class Tracker:
+    tracker = None
+    encoder = None
+    tracks = None
+
     def __init__(self):
-        # Store the center positions of the objects
-        self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
-        self.id_count = 0
+        max_cosine_distance = 0.4
+        nn_budget = None
+
+        encoder_model_filename = 'deep_sort/networks/mars-small128.pb'
+
+        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+        self.tracker = DeepSortTracker(metric)
+        self.encoder = gdet.create_box_encoder(encoder_model_filename, batch_size=1)
+
+    def update(self, frame, detections):
+        if len(detections) == 0:
+            self.tracker.predict()
+            self.tracker.update([])  
+            self.update_tracks()
+            return self.tracks
+
+        bboxes = np.asarray([d[:-1] for d in detections])
+        bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, 0:2]
+        scores = [d[-1] for d in detections]
+
+        features = self.encoder(frame, bboxes)
+
+        dets = []
+        for bbox_id, bbox in enumerate(bboxes):
+            dets.append(Detection(bbox, scores[bbox_id], features[bbox_id]))
+
+        self.tracker.predict()
+        self.tracker.update(dets)
+        self.update_tracks()
+        return self.tracks
+    
+
+    def update_tracks(self):
+        tracks = []
+        for track in self.tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            bbox = track.to_tlbr()
+
+            id = track.track_id
+
+            tracks.append(Track(id, bbox))
+
+        self.tracks = tracks
 
 
-    def update(self, objects_rect):
-        # Objects boxes and ids
-        objects_bbs_ids = []
 
-        # center point of object
-        for rect in objects_rect:
-            x, y, w, h = rect
-            cx = (x + x + w) // 2
-            cy = (y + y + h) // 2
 
-            # Find out if that object was detected already
-            same_object_detected = False
-            for id, pt in self.center_points.items():
-                dist = math.hypot(cx - pt[0], cy - pt[1])
+class Track:
+    track_id = None
+    bbox = None
 
-                if dist < 35:
-                    self.center_points[id] = (cx, cy)
+    def __init__(self, id, bbox):
+        self.track_id = id
+        self.bbox = bbox
 
-                    objects_bbs_ids.append([x, y, w, h, id])
-                    same_object_detected = True
-                    break
 
-            # New object is detected we assign the ID to that object
-            if same_object_detected is False:
-                self.center_points[self.id_count] = (cx, cy)
-                objects_bbs_ids.append([x, y, w, h, self.id_count])
-                self.id_count += 1
 
-        # Clean the dictionary by center points to remove IDS not used anymore
-        new_center_points = {}
-        for obj_bb_id in objects_bbs_ids:
-            _, _, _, _, object_id = obj_bb_id
-            center = self.center_points[object_id]
-            new_center_points[object_id] = center
-
-        # Update dictionary with IDs not used removed
-        self.center_points = new_center_points.copy()
-        return objects_bbs_ids
 
 import torch
 torch.cuda.set_device(0)
@@ -117,70 +141,79 @@ while True:
         y1=int(row[1])
         x2=int(row[2])
         y2=int(row[3])
-        
+        score=row[4]
         d=int(row[5])
         c=class_list[d]
        
         if 'bus' in c:
-            list.append([x1,y1,x2,y2])
+            list.append([x1,y1,x2,y2,score])
         elif 'car' in c:
-             list1.append([x1,y1,x2,y2])
+             list1.append([x1,y1,x2,y2,score])
         elif 'auto-rikshaw' in c:
-             list2.append([x1,y1,x2,y2])
+             list2.append([x1,y1,x2,y2,score])
         elif 'motor-cycle' in c:
-             list3.append([x1,y1,x2,y2])
+             list3.append([x1,y1,x2,y2,score])
         
-    bbox_idx=tracker.update(list)
-    bbox1_idx=tracker1.update(list1)
-    bbox2_idx=tracker2.update(list2)
-    bbox3_idx=tracker3.update(list3)
-    for bbox in bbox_idx:
-        x3,y3,x4,y4,id=bbox
+    bbox_idx=tracker.update(frame,list)
+    bbox1_idx=tracker1.update(frame,list1)
+    bbox2_idx=tracker2.update(frame,list2)
+    bbox3_idx=tracker3.update(frame,list3)
+  
+    for track in bbox_idx:
+        bbox=track.bbox
+        x3,y3,x4,y4=bbox
+        id=track.track_id
         cx=int(x3+x4)//2
         cy=int(y3+y4)//2
         if cy1<(cy+offset) and cy1>(cy-offset):
-           cv2.rectangle(frame,(x3,y3),(x4,y4),(0,255,0),2)
-           cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1)
+           cv2.rectangle(frame, (int(x3), int(y3)), (int(x4), int(y4)), (0, 255, 0), 2)
+           cvzone.putTextRect(frame, f'{id}', (int(x3), int(y3)), 1, 1)
            if bus.count(id)==0:
               bus.append(id)
 #CAR
-    for bbox1 in bbox1_idx:
-        x5,y5,x6,y6,id1=bbox1
+    for track1 in bbox1_idx:
+        bbox1=track1.bbox
+        x5,y5,x6,y6=bbox1
+        id1=track1.track_id
         cx2=int(x5+x6)//2
         cy2=int(y5+y6)//2
         if cy1<(cy2+offset) and cy1>(cy2-offset):
-           cv2.rectangle(frame,(x5,y5),(x6,y6),(0,255,0),2)
-           cvzone.putTextRect(frame,f'{id1}',(x5,y5),1,1)
+           cv2.rectangle(frame, (int(x5), int(y5)), (int(x6), int(y6)), (0, 255, 0), 2)
+           cvzone.putTextRect(frame, f'{id1}', (int(x5), int(y5)), 1, 1)
            if car.count(id1)==0:
               car.append(id1)
 #auto-rikshaw
-    for bbox2 in bbox2_idx:
-        x7,y7,x8,y8,id2=bbox2
+    for track2 in bbox2_idx:
+        bbox2=track2.bbox
+        x7,y7,x8,y8=bbox2
+        id2=track2.track_id
         cx3=int(x7+x8)//2
         cy3=int(y7+y8)//2
         if cy1<(cy3+offset) and cy1>(cy3-offset):
-           cv2.rectangle(frame,(x7,y7),(x8,y8),(0,255,0),2)
-           cvzone.putTextRect(frame,f'{id2}',(x7,y7),1,1)
+           cv2.rectangle(frame, (int(x7), int(y7)), (int(x8), int(y8)), (0, 255, 0), 2)
+           cvzone.putTextRect(frame,f'{id2}',(int(x7),int(y7)),1,1)
            if auto_rikshaw.count(id2)==0:
               auto_rikshaw.append(id2)
 #motorcycle
-    for bbox3 in bbox3_idx:
-        x9,y9,x10,y10,id3=bbox3
+    for track3 in bbox3_idx:
+        bbox3=track3.bbox
+        x9,y9,x10,y10=bbox3
+        id3=track3.track_id
         cx4=int(x9+x10)//2
         cy4=int(y9+y10)//2
         if cy1<(cy4+offset) and cy1>(cy4-offset):
-           cv2.rectangle(frame,(x9,y9),(x10,y10),(0,255,0),2)
-           cvzone.putTextRect(frame,f'{id3}',(x9,y9),1,1)
+           cv2.rectangle(frame, (int(x9), int(y9)), (int(x10), int(y10)), (0, 255, 0), 2)
+           cvzone.putTextRect(frame, f'{id3}', (int(x9), int(y9)), 1, 1)
            if motorcycle.count(id3)==0:
               motorcycle.append(id3)          
     countbus=(len(bus))
     countcar=(len(car))
     countauto_rikshaw=(len(auto_rikshaw))
     countmotorcycle=(len(motorcycle))
-    cvzone.putTextRect(frame,f'Auto_Rikshaw :-{countauto_rikshaw}',(50,60),scale=2,thickness=2,colorR=(0,0,255))
-    cvzone.putTextRect(frame,f'Bus :-{countbus}',(50,120),scale=2,thickness=2,colorR=(0,0,255))
-    cvzone.putTextRect(frame,f'Car :-{countcar}',(50,180),scale=2,thickness=2,colorR=(0,0,255))
-    cvzone.putTextRect(frame,f'MotorCycle :-{countmotorcycle}',(50,240),scale=2,thickness=2,colorR=(0,0,255))
+    cvzone.putTextRect(frame,f'Auto_Rikshaw : {countauto_rikshaw}',(50,60),scale=2,thickness=2,colorR=(0,0,255))
+    cvzone.putTextRect(frame,f'Bus : {countbus}',(50,120),scale=2,thickness=2,colorR=(0,0,255))
+    cvzone.putTextRect(frame,f'Car : {countcar}',(50,180),scale=2,thickness=2,colorR=(0,0,255))
+    cvzone.putTextRect(frame,f'MotorCycle : {countmotorcycle}',(50,240),scale=2,thickness=2,colorR=(0,0,255))
 
 
     cv2.line(frame,(405,427),(580,427),(255,255,255),2)
